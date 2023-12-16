47137c8a2cdabe5d668bc3383e7ea50c4dc699c3
BuildTools <unconfigured@null.spigotmc.org>
2023-12-16 18:39:20 +0100

    MCreator Merge commit 'b1ebd785f73d4b77ab1e2f91760024758c922335' into b1cc0f6ab08574303cb6d818b51dd0091de7d82f

--- src/main/java/net/mcreator/moregenerators/init/MoreGeneratorsModItems.java
+++ src/main/java/net/mcreator/moregenerators/init/MoreGeneratorsModItems.java
@@ -20,6 +20,7 @@ import net.mcreator.moregenerators.MoreGeneratorsMod;
 public class MoreGeneratorsModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MoreGeneratorsMod.MODID);
 	public static final RegistryObject<Item> BIOFUEL = REGISTRY.register("biofuel", () -> new BiofuelItem());
+	public static final RegistryObject<Item> SSS = block(MoreGeneratorsModBlocks.SSS);
 	public static final RegistryObject<Item> OIL_BUCKET = REGISTRY.register("oil_bucket", () -> new OilItem());
 	public static final RegistryObject<Item> BIOMASS_BURNER = block(MoreGeneratorsModBlocks.BIOMASS_BURNER);
 	public static final RegistryObject<Item> ENERGY_METER = REGISTRY.register("energy_meter", () -> new EnergyMeterItem());
